09ff5c5824f1717311eb0de8901a4599165a326a
M-123-456 <mikigerlach2@gmail.com>
2022-03-09 13:10:56 +0100

    live-coding finished

--- edward.js
+++ edward.js
@@ -25,7 +25,7 @@ AUTHOR: ${this.author}
 NUMBER OF PAGES: ${this.numberOfPages}`;
   }
 
-  // factory pattern - static enables the function to be called by class and not object
+  // factory pattern
   static instantiateBook(title, author, numberOfPages) {
     return new Book(title, author, numberOfPages);
   }
@@ -59,19 +59,23 @@ class Magazine extends Publication {
     return `TITLE: ${this.title}, ISSUE NUMBER: ${this.issueNumber}, NUMBER OF PAGES: ${this.numberOfPages}`;
   }
 }
-
-const book = new Book("Sapiens", "Yuval Harari", 543);
+// code smell
+const book = Book.instantiateBook("Sapiens", "Yuval Harari", 543);
 const book2 = new Book(
-  "JavaScript Functional Programming",
+  "JavaScript Functional Programmign",
   "Federico Kereki",
   433
 );
+// console.log(book.displayAsLine());
+// console.log(book2.displayAsBlock());
+// console.log(book.displayAsBlock());
+
 const books = new Books();
 books.addBook(book);
 books.addBook(book2);
+// console.log(books.listBooks());
+
 const magazine = new Magazine("Spiegel", 34, 543);
 console.log(book.displayAsLine());
 console.log(magazine.displayAsLine());
-
 console.log(books.getBooksAsArray());
-// console.log(books.listBooks());
